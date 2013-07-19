0d34e25ea127802d23391b2ced2b478df4a79949
Sun Ning <nsun@redhat.com>
2013-07-19 15:47:31 +0800

    for script longevity_sshkey.sh

--- longevity/longevity_sshkey.sh
+++ longevity/longevity_sshkey.sh
@@ -5,12 +5,12 @@ ssh_auth_config
 logfile=./log/sshkey_$(date +%Y%m%d-%H%M%S).log
 rhc sshkey list -l $user -p $passwd > /dev/null
 if [ $? -eq 0 ];then
-    cur_sshkey=`rhc sshkey list -l $user -p $passwd|grep type |awk 'print $1'`
+    cur_sshkey=`rhc sshkey list -l $user -p $passwd|grep type |awk '{print $1}'`
     rhc sshkey remove $cur_sshkey
 fi
 testname=test001
 number=0
-while ture;do 
+while true;do 
     echo "# $number add test, date: $(date +%Y%m%d-%H%M%S)"
     rhc sshkey add $testname ~/.ssh/id_rsa.pub -l $user -p $passwd
     rhc sshkey remove $testname -l $user -p $passwd
